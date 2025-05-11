9f0a9e9b2dec553b1de69448d8a2befbf39796e2
oumaoumag <ouma.godwin10@gmail.com>
2025-05-11 23:01:28 +0300

    Add MockERC20 contract for testing in Escrow tests

---
+++ test/Escrow.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-0.3
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Escrow.sol";
+
+// mock ERC20 token for testing
+contract MockERC20 {
+    // TODO
+}
